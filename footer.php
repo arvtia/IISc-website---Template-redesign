e86d5fd821520cb11418171cf66f3c04accbfd21
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-21 16:05:34 +0530

    index page , header and footer

---
+++ footer.php
@@ -0,0 +1,105 @@
+<footer>
+    <!-- everything binds up inthe container- -->
+    <div class="everything-container-liquidity-solity">
+
+        <!-- div with image globe -->
+        <div class="main-footer-body-map">
+            <img src="assests/images/map-globe-footer.png" alt="map-globe-footer" id="footer-map-global">
+        </div>
+
+        <!-- second-div to be displayed on top -->
+        <div class="main-footer-body-map-overlay">
+            <div class="text-box-of" >
+                <h1>Top cities where our alumni live</h1>
+            </div>
+            
+            <div class="row-of-cities">
+                    <div class="row-of-cities-colums" style="display: flex;">
+                        <div class="images-rw-c-c">
+                            <img src="assests/images/city-icon.png" alt="">
+                        </div>
+                        <div class="images-descriptions-rw-c-c">
+                            <p style="font-size: 1rem; color: white; margin: 5px;">6072 alumni</p>
+                            <p style="color: orange; font-size: 1.3rem; margin: 5px;">Bengaluru</p>
+                        </div>
+                    </div>
+                    <div class="row-of-cities-colums" style="display: flex;">
+                        <div class="images-rw-c-c">
+                            <img src="assests/images/city-icon.png" alt="">
+                        </div>
+                        <div class="images-descriptions-rw-c-c">
+                            <p style="font-size: 1rem; color: white; margin: 5px;">6072 alumni</p>
+                            <p style="color: orange; font-size: 1.3rem; margin: 5px;">Bengaluru</p>
+                        </div>
+                    </div>
+                    <div class="row-of-cities-colums" style="display: flex;">
+                        <div class="images-rw-c-c">
+                            <img src="assests/images/city-icon.png" alt="">
+                        </div>
+                        <div class="images-descriptions-rw-c-c">
+                            <p style="font-size: 1rem; color: white; margin: 5px;">6072 alumni</p>
+                            <p style="color: orange; font-size: 1.3rem; margin: 5px;">Bengaluru</p>
+                        </div>
+                    </div>
+            </div>
+                <div class="footer-button-ahref">
+                    <a href="#">Explore on Map</a>
+                </div>
+        </div>
+        <!-- div.container -wrapper ends here -->
+    </div>
+    <div class="footer-container-liquid-lol">
+        <div class="contaminatior-lol-pick-it-up">
+            <div class="muito-obrigado">
+                <p>BROWSE</p>
+                <P>By Search</P>
+                <p>By Map</p>
+                <p>By Resources</p>
+            </div>
+            <div class="muito-obrigado">
+                <p>LINKS</p>
+                <P>IISc</P>
+                <p>IISc Alumni Association</p>
+                <p>IISc AANA</p>   
+            </div>
+            <!-- Events
+IISc Annual Alumni Reunion 2024 -->
+            <div class="muito-obrigado">
+                <p>EVENTS</p>
+                <p>IISc Annual Alumni Reunion 2024</p>
+            </div>
+            <div class="muito-obrigado">
+                <p class="mutio-obrigado-contact-us" style="font-style: italic;">Connect to us</p>
+                <P>alumniaffairs.odaa@iisc.ac.in</P>
+                <p>+91 80 2293 3590 / 3591</p>
+                <p>By Resources</p>
+                <br>
+                <p>Privacy Policy</p>
+                <p>Terms of Use</p>
+            </div>
+        </div>
+    </div>
+    <!-- footer-bottom- -->
+    <div class="footer-base-liquifiy-it-mann">
+        <div class="footer-ki-base-container">
+            <div class="f-c-b-l-i-mann">
+                <img src="assests/images/Transparent_IISc_Logo.png" alt="logo" id="logo-ab-footermebhi">
+                <p> © Copyright 2025</p>
+            </div>
+            <div class="f-c-b-l-i-mann2">
+                <p>Powered by IZS Techocrates Pvt Ltd</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<script src="assests/script/headerfooter.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include ("header.php") ?>
+
+
+
+
+
+
+
+<div class="docs" style="height: 100vh"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include ("footer.php") ?>
