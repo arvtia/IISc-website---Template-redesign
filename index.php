44703e430cae86a1519b74151393d0992a725bd5
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-02-22 15:25:48 +0530

    Update index.php
    Updated contents on  main page

--- index.php
+++ index.php
@@ -1,14 +1,333 @@
 <?php include ("header.php") ?>
 
 
+<div class="container-fluid ">
+    <div class="row pb-5" style="position: relative; top: -47px">
+
+   <!--  bigger-column -->
+        <div class="col-md-0 col-lg-2 col-sm-0 custom-side-bar">
+
+        </div>
+    <!-- empty space -->
+
+        <div class="col-lg-6  col-md-8 col-sm-10 mx-auto">
+          <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+                        <img src="assests/images/ca-1.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+                        <img src="assests/images/ca-2.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+                       <img src="assests/images/ca-1.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px; ">
+                        <img src="assests/images/ca-3.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px; ">
+                        <img src="assests/images/ca-2.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+                        <img src="assests/images/ca-4.jpg" alt="" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 500px;">
+                        <img src="assests/images/ca-5.jpg" alt="" class="img-fluid w-100 object-fit-cover ">
+                    </div>
+                </div>
+                </div>
+
+                <!-- Previous & Next Buttons -->
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
 
 
+        <!-- smaller - column -->
+        <div class="col-lg-2 col-md-8 col-sm-10 mx-auto custom-color-bg pt-2">
+            <div class="row w-100 justify-content-center mx-auto">
+                <div class="text-center py-4 text-white">
+                    Login in / Sign Up
+                </div>
+                <div class="text-center py-3 text-white">
+                    New Features
+                </div>
+                <hr>
+                <div class="row">
+
+                    <div class="col-6">
+                        <img src="assests/images/gallery-icon.png" class="img-fluid"  alt="">
+                    </div>
+                    <div class="col-6">
+                        <img src="assests/images/debitcard.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-6">
+                        <img src="assests/images/debitcard.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-6">
+                        <img src="assests/images/debitcard.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <!-- empty space  -->
+        <div class="col-md-0 col-lg-2 col-sm-0 custom-side-bar">
+
+        </div>
+        <!-- ends here -->
+    </div>
+</div>
 
 
+<!-- <div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="col-md-8 col-sm-10 bg-white">
+                <div class="text-dark d-flex justify-content-between py-3">
+                    <p class="fs-5 px-3">Jobs and Internships</p>
+                    <p class="fs-6 text-warning px-3">View all Jobs</p>
+                </div>
+                <div class="row">
+                <div class="d-flex flex-column align-items-start">
+                    <p class="text-warning fs-6 custom-font m-0">Mechanical Design Engineer | Applied Materials</p>
+                    <p class="fw-bold m-0">Location: <span class="text-dark">Bengaluru</span></p>
+                    <p class="fw-bold m-0">Salary: <span class="text-dark">Competitive</span></p>
+                    <button class="btn btn-warning mt-2">Apply Now</button>
+                </div>
 
-<div class="docs" style="height: 100vh"></div>
 
+                    
+                    <div class="col-md-5">
+                        <p class="text-warning align-items-center fs-6 custom-font">Mechanical Design Engineer | Applied Materials</p>
+                        <strong>Location:</strong> Bengalooru
+                        Salary:
+                        <br>
+                        <div class="btn bg-warning">Apply</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 col-sm-10">
 
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            
+        </div>
+    </div>
+</div> -->
+
+<div class="row bg-success bg-transparent" style="position: relative; top: -80px;">
+    <div class="col-8 bg-transparent mx-auto">
+        <div class="row">
+            <!-- smaller - content -->
+            <div class="col-md-9 bg-transparent">
+                <!-- Header Section -->
+                <div class="bg-white d-flex pt-3 px-2 border-bottom ">
+                    <div class="col-8 bg-white">
+                        <p class="fs-5 ps-2 fw-bold">JOBS AND INTERNSHIPS</p>
+                    </div>
+                    <div class="col-4 text-end">
+                        <p class="fs-6 pe-2">View Jobs</p>
+                    </div>
+                </div>
+                    <!-- Job Listings -->
+                    <div class="row g-0 d-flex justify-content-between bg-white py-2 px-2">
+                        <!-- Left Column -->
+                        <div class="col-6 g-2 px-2">
+                            <div>
+                                <div class="text-start d-flex" >
+                                    <p class="fs-6 text-warning fw-bold">Mechanical Design Engineer |</p><p class="text-dark fs-6 fst-italic">Applied Materials</p>
+                                </div>
+                                <p class="fs-6">Location: Bengalooru</p>
+                                <p class="fs-6">Salary</p>
+                                <button class="btn btn-warning py-1 px-3 mb-3 rounded-0"> Apply</button>
+                            </div>
+                        </div>
+                        <!-- Right Column -->
+                        <div class="col-6 g-2 px-2">
+                            <div>
+                                <div class="text-start d-flex" >
+                                    <p class="fs-6 text-warning fw-bold">Mechanical Design Engineer |</p><p class="text-dark fs-6 fst-italic">Applied Materials</p>
+                                </div>
+                                <p class="fs-6">Location: Bengalooru</p>
+                                <p class="fs-6">Salary</p>
+                                <button class="btn btn-warning py-1 px-3 mb-3 rounded-0"> Apply</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid bg-white mt-3">
+                        <div class="row g-0 bg-white">
+                            <div class="text-dark d-flex justify-content-between pt-2">
+                                <h4 class="ps-3">Gallery</h4>
+                                <p class="pe-3">Go to Gallery</p>
+                            </div>
+                            <div class="colum-11 px-3 pb-3 d-flex mx-auto">
+                                <img src="assests/images/g1.jpg" alt="g-1" class="image-fluid w-100 h-100 object-fit-contain">
+                            </div>
+
+                            <div class="container-fluid mb-5 align-items-center d-flex" >
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="assests/images/g3.jpg" alt="g3" class="img-fluid object-fit-cover">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="assests/images/g6.jpg" alt="g2" class="img-fluid object-fit-cover" id="image-gallery-issued">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="assests/images/g4.jpg" alt="g4" class="img-fluid object-fit-cover">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>
+                        
+                        <!-- lolwa page
+                        <div class="container-fluid mb-5">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="assests/images/g3.jpg" alt="g3" class="img-fluid object-fit-cover">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="assests/images/g6.jpg" alt="g2" class="img-fluid object-fit-cover" id="image-gallery-issued">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="assests/images/g4.jpg" alt="g4" class="img-fluid object-fit-cover">
+                                    </div>
+                                </div>
+                            </div>
+                        </div> -->
+
+                    </div>
+            <div class="col-3 bg-transprant ps-0 pe-0">
+            <div class="container-fluid bg-white w-100">
+                <div class="row">
+                     <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center w-100">
+                        <h5 class="py-3">LATEST NEWS</h5>
+                        <hr class="w-100 mx-auto">
+                        <p class="fs-6 " style="color:#CF6F3F"><small>FEB 21</small></p>
+                        <p class="fst-italic w-100" ><small><u>Winners of Alumni Instituted Travel Awards</u></small></p>
+                        <hr class="w-50 mx-auto" style="background-color: solid black; height: 3px; border: 0;">
+                        <p class="fs-6 " style="color:#CF6F3F"><small>FEB 08</small></p>
+                        <p class="fst-italic w-100"><small><u>Alumnus Dr S Unnikrishnan Nair taken charge as Director, VSSC</u></small></p>
+                        <hr class="w-50 mx-auto" style="background-color:black; height: 3px; border: 0;">
+                        <p class="fs-6 " style="color:#CF6F3F"><small>JAN 18</small></p>
+                        <p class="fst-italic w-100"><small><u>Alumnus Shri S Somanath appointed next ISRO Chair</u></small></p>
+                     </div>
+                </div>
+            </div>
+            <!-- another div below it -->
+            <div class="text-center py-3 bg-transprant">
+                <div class="container-fluid bg-white w-100 pt-3 position-relative">
+                    <div class="row">
+                        <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center w-100 position-relative z-1">
+                            <h5 class="pt-3">EVENTS</h5>
+                            <hr class="w-100 mx-auto">
+                                <div class="w-100 ">
+                                    <div class="text-white d-flex py-4" style="background-color:#CF6F3F">
+                                        <h1 class="px-2">20</h1>
+                                        <div class="vr"></div>
+                                        <p class="fst-italic text-white fs-6 py-2 px-2 my-auto">December 2024 friday</p>
+                                    </div>
+                                    <div class="text-center" style="background-color:#E4E4E4;">
+                                        <p class="fs-6 pt-2" style="color: #CF6F3F;">IISc Annual Alumni Reunion 2024</p>
+                                        <p class="text-dark p-0 m-0">From 9 pm</p>
+                                        <hr class="w-50 mx-auto" style="background-color: black; height: 3px; border: 0;">
+                                        <p class="fs-6 fst-italic pb-3 "><small>Indian Institute of Science</small></p>
+                                    </div>
+                                </div>
+                            <p class="fs-6 " style="color:#CF6F3F"><small>View All</small></p>
+                        </div>
+                        <!-- <div class="col-12 d-flex flex-column  w-100 position-absolute z-2">
+                            
+                        </div> -->
+
+                    </div>
+                </div>
+            </div>
+            <!-- another div below it -->
+            <div class="text-center pb-3 bg-transprant">
+                <div class="container-fluid bg-white w-100 pt-3 position-relative">
+                    <div class="row">
+                        <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center w-100 position-relative z-1">
+                            <h5 class="pt-3">STORY</h5>
+                            <hr class="w-100 mx-auto">
+                            <p class="fs-6 fst-italic w-75 lh-1 " style="color:#CF6F3F ">POISSONS THEORY OF ELASTIC PLATES</p>
+                            <p class="fs-6 fst-italic w-100 lh-1 " style="color:black">By GIRISH KUMAR RAMAIAH GIRISH KUMAR RAMAIAH</p>
+                            <hr class="w-50 mx-auto p-0" style="background-color:black; height: 3px; border: 0;">
+                            <p class="fs-6 " style="color:#CF6F3F"><small>Introduction</small></p>
+                            <p class="fst-italic w-100"><small>
+                            This ground breaking book resolves the main lacuna in Kirchhoff theory of bending of plates in the Poisson-Kirchhoff boundary conditions paradox through the introduction of auxiliary problem governing transverse stresses. The book highlights new primary bending problem which is formulated and analyzed by the application of developed Poisson theory. Analysis with prescribed transverse stresses along faces of the plate,...</small></p>
+                            <p class="fs-6 " style="color:#CF6F3F"><small>Read full story</small></p>
+                        </div>
+                        <div class="col-12 d-flex flex-column  w-100 position-absolute z-2">
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="text-center bg-transprant">
+                <div class="container-fluid bg-white w-100 pt-3 position-relative">
+                    <div class="row">
+                        <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center w-100 position-relative z-1">
+                            <h5 class="pt-3">STUDENT GUIDELINES</h5>
+                            <hr class="w-100 mx-auto">
+                            <hr class="w-50 mx-auto p-0" style="background-color:black; height: 3px; border: 0;">
+                            <p class="fs-6 text-dark"><small><strong>All students must follow these guidelines for interacting with alumni through the alumni website.</strong></small></p>
+                            <ol class="list-group list-group-numbered text-start">
+                                <li class="list-group-item"><small>Students must not share the contact details of any alumnus/alumna with anyone.</small></li>
+                                <li class="list-group-item"><small>Students shall provide all the necessary information requested by Alumni for specific purposes.</small></li>
+                            </ol>
+
+                            <p class="fs-6 " style="color:#CF6F3F"><small>Read full story</small></p>
+                        </div>
+                        <div class="col-12 d-flex flex-column  w-100 position-absolute z-2">
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+            <!-- right side container  fluid ends here -->
+        
+            <!-- another one last one probably  -->
+
+        </div>
+        <div class="row bg-success">
+            
+            <div class="col-4">
+                <h1>lolwa</h1>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
